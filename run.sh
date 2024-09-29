8af7f7f5afe1cadf79f1dbf9192c31a07881698f
Barchfeld <joaovitorbarchfelld@gmail.com>
2024-09-29 20:34:01 -0300

    Edit 5

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 exec1="num_primos_paralelo_normal"
-exec2="num_primos_fila_concorrente_normal"
+#exec2="num_primos_fila_concorrente_normal"
 saveLog1="logs/$exec1"
 saveLog2="logs/$exec2"
 problemSize=1000000
-for ((i=1; i<=2; i++)) #10 repetitions
+for ((i=1; i<=1; i++)) #10 repetitions
 do
     for j in {1..12..1} #from 2 to 12 threads
     do
